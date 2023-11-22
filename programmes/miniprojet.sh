70327504eed37c75ab228edf89d39845c30bfa1e
valentinefleith <valentine.fleith@gmail.com>
2023-11-22 09:54:21 +0100

    modification programme pour ecriture dans le fichier + test sur les urls chinois

--- programmes/miniprojet.sh
+++ programmes/miniprojet.sh
@@ -12,39 +12,40 @@ if [ ! -f "$URLS" ]; then
     exit
 fi
 
-echo "<html>"
-echo "<head>"
-echo "    <meta charset=\"UTF-8\">"
-echo "    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">"
-echo "    <style>"
-echo "        .table-container {"
-echo "            max-width: 1000px;"
-echo "            margin: 0 auto;"
-echo "        }"
-echo "        .has-background-rose {"
-echo "            background-color: #FFE7FE;"
-echo "        }"
-echo "    </style>"
-echo "</head>"
-echo "<body>"
-echo "    <div class=\"container\">"
-echo "        <h1 class=\"title is-1 has-text-centered has-text-grey\"><b>Tableau</b></h1>"
-echo "        <div class=\"table-container\">"
-echo "            <table class=\"table is-bordered is-striped is-hoverable is-fullwidth\">"
-echo "                <tr class=\"has-background-rose\">"
-echo "                    <th>Numero ligne</th><th>URL</th><th>Code HTTP</th><th>Encodage</th>"
-echo "                </tr>"
+OUTPUT_FILE="../tableaux/tableau_chinois.html"
+echo "<html>
+<head>
+    <meta charset=\"UTF-8\">
+    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">
+    <style>
+        .table-container {
+            max-width: 1000px;
+            margin: 0 auto;
+        }
+        .has-background-rose {
+            background-color: #FFE7FE;
+        }
+    </style>
+</head>
+<body>
+    <div class=\"container\">
+        <h1 class=\"title is-1 has-text-centered has-text-grey\"><b>Tableau</b></h1>
+        <div class=\"table-container\">
+            <table class=\"table is-bordered is-striped is-hoverable is-fullwidth\">
+                <tr class=\"has-background-rose\">
+                    <th>Numero ligne</th><th>URL</th><th>Code HTTP</th><th>Encodage</th>
+                </tr>" > $OUTPUT_FILE
 lineno=1
 while read -r URL; do
     response=$(curl -s -I -L -w "%{http_code}" -o /dev/null "$URL")
-    encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null "$URL" | ggrep -E -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
-    echo "                <tr>"
-    echo "                    <td>$lineno</td><td>$URL</td><td>$response</td><td>$encoding</td>"
-    echo "                </tr>"
+    encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null "$URL" | egrep -E -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
+    echo "<tr>
+				<td>$lineno</td><td>$URL</td><td>$response</td><td>$encoding</td>
+		</tr>" >> $OUTPUT_FILE
     lineno=$(expr $lineno + 1)
 done < "$URLS"
-echo "            </table>"
-echo "        </div>"
-echo "    </div>"
-echo "</body>"
-echo "</html>"
+echo "            </table>
+        </div>
+    </div>
+</body>
+</html>" >> $OUTPUT_FILE
