801de77cb06268a9f16ef9a156f9353cfe84451f
valentinefleith <valentine.fleith@gmail.com>
2023-11-26 22:49:03 +0100

    ajout contextes

--- programmes/creation_tableaux.sh
+++ programmes/creation_tableaux.sh
@@ -47,20 +47,28 @@ echo "<html>
         <div class=\"table-container\">
             <table class=\"table is-bordered is-striped is-hoverable is-fullwidth\">
                 <tr class=\"has-background-rose\">
-                    <th>Numero ligne</th><th>URL</th><th>Aspiration</th><th>Dump</th><th>Code HTTP</th><th>Encodage</th><th>Compte</th>
+                    <th>Numero ligne</th><th>URL</th><th>Aspiration</th><th>Dump</th><th>Contexte</th><th>Code HTTP</th><th>Encodage</th><th>Compte</th>
                 </tr>" > $OUTPUT_FILE
 lineno=1
 while read -r URL; do
 	FICHIER_ASPIRATION="../aspirations/${LANGUE}/aspiration${lineno}.html"
+
     response=$(curl -s -L -w "%{http_code}" -o "$FICHIER_ASPIRATION" "$URL")
     encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null "$URL" | egrep -E -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
+
 	FICHIER_DUMP="../dump-texts/${LANGUE}/dump${lineno}.html"
 	lynx -dump -assume_charset=$encoding $URL > $FICHIER_DUMP
-	echo $MOT
+
 	COMPTE=$(egrep -o "$MOT" $FICHIER_DUMP | wc -l)
+
+	CONTEXTE=$(egrep -A2 -B2 "$MOT" $FICHIER_DUMP)
+	FICHIER_CONTEXTE="../contextes/${LANGUE}/contexte${lineno}.html"
+	echo $CONTEXTE > $FICHIER_CONTEXTE
+
     echo "<tr>
-				<td>$lineno</td><td>$URL</td><td><a href='$FICHIER_ASPIRATION'>Aspiration</a></td><td><a href='$FICHIER_DUMP'>Dump</a></td><td>$response</td><td>$encoding</td><td>$COMPTE</td>
+				<td>$lineno</td><td>$URL</td><td><a href='$FICHIER_ASPIRATION'>Aspiration</a></td><td><a href='$FICHIER_DUMP'>Dump</a></td><td><a href='$FICHIER_CONTEXTE'>Contexte</a></td><td>$response</td><td>$encoding</td><td>$COMPTE</td>
 		</tr>" >> $OUTPUT_FILE
+
     lineno=$(expr $lineno + 1)
 	echo "OK"
 done < "$URLS"
