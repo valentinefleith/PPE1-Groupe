a0a578efb3225dbe7f5e5da9ff20aef2f541f5a3
Lidan Zhang <zhanglidan2002@gmail.com>
2023-12-13 17:13:04 +0100

    update stop word chinois et wordcloud

--- programmes/tokeniser.py
+++
@@ -1,7 +0,0 @@
-import thulac
-
-thul = thulac.thulac(seg_only = True)
-
-thul.cut_f("../itrameur/contextes-zh.txt", "../itrameur/tokenized-contextes.txt")
-
-thul.cut_f("../itrameur/dump-texts-zh.txt", "../itrameur/tokenized-dump.txt")
